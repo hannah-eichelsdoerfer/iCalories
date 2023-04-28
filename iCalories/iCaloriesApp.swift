bcc13567a7ee2900a8c2eab14274e9eb3ee3e1d9
Hannah Eichelsdoerfer <hannaheichelsdoerfer@gmail.com>
2023-04-28 18:03:26 +1000

    Initial Commit

---
+++ iCalories/iCaloriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  iCaloriesApp.swift
+//  iCalories
+//
+//  Created by Hannah on 28/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct iCaloriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
